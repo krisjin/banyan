1804163a156e751ef5ceae5482b1d8aa9857c9a4
krisjin <krisibm@163.com>
2015-02-03 12:47:47 +0800

    ReentrantLock

---
+++ src/main/java/com/concurrent/demo03/Test.java
@@ -0,0 +1,36 @@
+package com.concurrent.demo03;
+
+public class Test {
+
+	public static void main(String[] args) {
+		BufferInterruptibly buffer = new BufferInterruptibly();
+
+		final Write write = new Write(buffer);
+		final Read read = new Read(buffer);
+
+		final Thread wThread = new Thread(write);
+		final Thread rThread = new Thread(read);
+		wThread.start();
+		rThread.start();
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+
+				long startDate = System.currentTimeMillis();
+
+				for (;;) {
+					if (System.currentTimeMillis() - startDate > 5000) {
+						System.out.println("不等了，尝试终端。");
+						rThread.interrupt();
+						break;
+					}
+				}
+
+			}
+		}).start();
+
+	}
+
+}
---
+++ src/main/java/com/concurrent/demo03/Write.java
@@ -0,0 +1,15 @@
+package com.concurrent.demo03;
+
+public class Write implements Runnable {
+	private BufferInterruptibly buffer;
+
+	public Write(BufferInterruptibly buffer) {
+		this.buffer = buffer;
+	}
+
+	@Override
+	public void run() {
+		buffer.write();
+	}
+
+}

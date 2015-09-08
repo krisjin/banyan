30141a7d2b24c51483a504d1e0884e9a93f60a68
krisjin <krisibm@163.com>
2015-09-08 13:26:31 +0800

    thread interrupt

---
+++ src/main/java/com/concurrent/interrupt/SleepInterrupt.java
@@ -0,0 +1,45 @@
+package com.concurrent.interrupt;
+
+/**
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/9/8
+ * Time: 13:17
+ */
+public class SleepInterrupt implements Runnable {
+    @Override
+    public void run() {
+
+        try {
+            System.out.println("run() sleep 10 seconds");
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println("run() - interrupted while sleeping");
+            //处理完中断异常后，返回到run（）方法人口，
+            //如果没有return，线程不会实际被中断，它会继续打印下面的信息
+            return;
+        }
+
+        System.out.println("run() - leaving normally");
+    }
+
+
+    public static void main(String[] args) {
+
+        SleepInterrupt sleepInterrupt = new SleepInterrupt();
+        Thread thread = new Thread(sleepInterrupt);
+        thread.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("main() - interrupting other thread");
+
+        thread.interrupt();
+
+        System.out.println("main() - end");
+    }
+
+}
--- src/main/java/com/concurrent/reentrant/Main.java
+++ src/main/java/com/concurrent/reentrant/Main.java
@@ -3,7 +3,8 @@ package com.concurrent.reentrant;
 /**
  * Java 提供的同步机制支持锁重入，以避免死锁。
  * 当一个线程持有对象锁时，当再尝试去持有已经占有的锁时，会以线程的方式重新持有对象锁。
- * 当下的WeixinPay继承了Pay,在WeixinPay中重写了pay方法，并调用父类中的pay方法，因为两次调用的pay方法都是同步的。
+ * 当下的WeixinPay继承了Pay,在WeixinPay中覆写了父类的pay方法，并调用父类中的pay方法，因为两次调用的pay方法都是同步的，
+ * 如果没有可重入的锁，那么这段代码会产生死锁
  * <p/>
  * User : krisibm@163.com
  * Date: 2015/9/7
@@ -12,7 +13,6 @@ package com.concurrent.reentrant;
 public class Main {
 
     public static void main(String[] args) {
-
         WeixinPay weixinPay = new WeixinPay();
         weixinPay.pay();
     }

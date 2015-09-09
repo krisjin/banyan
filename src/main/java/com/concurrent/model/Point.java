193814233fb13447a87b859d949016d313f6e83c
krisjin <krisibm@163.com>
2015-09-09 22:54:57 +0800

    将线程安全委托给ConcurrentHashMap

---
+++ src/main/java/com/concurrent/model/Point.java
@@ -0,0 +1,21 @@
+package com.concurrent.model;
+
+/**
+ * 不可变的Point类,所以是线程安全的。
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/9/9
+ * Time: 22:42
+ */
+public class Point {
+
+    public final int x;
+
+    public final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}

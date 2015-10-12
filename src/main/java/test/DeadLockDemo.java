3a7df8ccb2765ae9a8aa868435f82e2dc1e73425
krisjin <krisibm@163.com>
2015-10-12 22:14:15 +0800

    死锁

---
+++ src/main/java/test/DeadLockDemo.java
@@ -0,0 +1,50 @@
+package test;
+
+/**
+ * 死锁场景
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/10/12
+ * Time: 22:00
+ */
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.currentThread().sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("1");
+                    }
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+
+}
---
+++ src/main/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/10/12
+ * Time: 9:57
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 16/16;
+        System.out.println(i);
+    }
+}

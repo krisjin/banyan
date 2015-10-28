a8a9afbbaa5ecec82885842d10de35e974f399f3
krisjin <krisibm@163.com>
2015-10-28 17:25:55 +0800

    update future

--- src/main/java/com/concurrent/task/CallableFutureResult.java
+++ src/main/java/com/concurrent/task/CallableFutureResult.java
@@ -1,6 +1,9 @@
 package com.concurrent.task;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * <p/>
@@ -11,21 +14,16 @@ import java.util.concurrent.*;
 public class CallableFutureResult {
 
     public static void main(String[] args) {
+
         ExecutorService executorService = Executors.newCachedThreadPool();
+
         Task task = new Task();
 
         Future<Integer> future = executorService.submit(task);
 
         executorService.shutdown();
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         System.out.println("主线程在执行任务");
-
         try {
             Integer result = future.get();
             System.out.println("task execute result is : " + result);

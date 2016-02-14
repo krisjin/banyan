75fcd404be2cb66e9a3af6f888cbc1a3f786272a
shijingui <shijingui@jd.com>
2016-02-14 11:01:49 +0800

    WaitNotify

--- src/main/java/com/concurrent/thread/WaitNotify.java
+++ src/main/java/com/concurrent/thread/WaitNotify.java
@@ -7,6 +7,9 @@ package com.concurrent.thread;
  */
 public class WaitNotify {
 
+    static volatile boolean flag = true;
+    static Object obj = new Object();
+
     public static void main(String... args) {
         Thread waitThread = new Thread(new Wait(), "WaitThread");
         waitThread.start();
@@ -20,11 +23,13 @@ public class WaitNotify {
         @Override
         public void run() {
             try {
-                System.out.println("WaitThread is waiting start....");
-                synchronized (WaitNotify.class) {
-                    WaitNotify.class.wait();
+                while (flag) {
+                    synchronized (obj) {
+                        System.out.println(Thread.currentThread().getName() + "  is waiting..... ");
+                        obj.wait();
+                    }
                 }
-                System.out.println("WaitThread is waiting end....");
+                System.out.println(Thread.currentThread().getName() + "  is done.... ");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -35,8 +40,12 @@ public class WaitNotify {
     static class Notify implements Runnable {
         @Override
         public void run() {
-            synchronized (WaitNotify.class) {
-                WaitNotify.class.notifyAll();
+            synchronized (obj) {
+                //获取obj的锁，然后进行通知，通知时不会释放obj的锁
+                obj.notifyAll();
+                System.out.println(Thread.currentThread().getName() + " is notify done....");
+//                SleepUtil.second(1);
+                flag = false;
             }
         }
     }

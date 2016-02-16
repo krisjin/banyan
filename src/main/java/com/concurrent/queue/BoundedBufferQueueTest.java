7bcf2357b269485689126642bdf4467decf5c7d6
shijingui <shijingui@jd.com>
2016-02-16 10:11:43 +0800

    有界阻塞队列

---
+++ src/main/java/com/concurrent/queue/BoundedBufferQueueTest.java
@@ -0,0 +1,66 @@
+package com.concurrent.queue;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * User: shijingui
+ * Date: 2016/2/16
+ */
+public class BoundedBufferQueueTest {
+
+    public static void main(String[] args) {
+
+        BoundedBufferQueue<Integer> queue = new BoundedBufferQueue<>(1200);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        executorService.execute(new Producer(queue));
+        executorService.execute(new Consumer(queue));
+
+    }
+
+    static class Producer implements Runnable {
+
+        private BoundedBufferQueue queue;
+
+        public Producer(BoundedBufferQueue queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                int num = (int) (Math.random() * 100);
+                try {
+                    queue.put(num);
+                    System.out.println(Thread.currentThread().getName() + " producer " + num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+
+        private BoundedBufferQueue queue;
+
+        public Consumer(BoundedBufferQueue queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Integer num = (Integer) queue.take();
+                    System.out.println(Thread.currentThread().getName() + " consume " + num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}

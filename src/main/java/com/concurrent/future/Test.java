7f1ce60515ce48c4412b5554f604016147a23aee
shijingui <shijingui@jd.com>
2017-05-06 09:59:10 +0800

    update test

--- src/main/java/com/concurrent/future/Test.java
+++ src/main/java/com/concurrent/future/Test.java
@@ -12,9 +12,10 @@ public class Test {
         //这里会立即返回，因为得到的是一个FutureData而不是RealData
         Data data = client.request("name");
         System.out.println("请求完毕...");
-        //
-//        SleepUtil.second(2);
-
+        //使用sleep代替其它业务操作
+        SleepUtil.second(1);
+        System.out.println("其它业务执行完毕...");
         System.out.println("data=" + data.getResult());
+
     }
 }

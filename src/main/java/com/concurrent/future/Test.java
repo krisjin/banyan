e3ec29f84963487cc3f464ab256fd3aca910ed2e
shijingui <shijingui@jd.com>
2017-05-06 08:00:46 +0800

    thread  future

---
+++ src/main/java/com/concurrent/future/Test.java
@@ -0,0 +1,20 @@
+package com.concurrent.future;
+
+import com.concurrent.util.SleepUtil;
+
+/**
+ * User shijingui
+ * Date 2017/5/6
+ */
+public class Test {
+    public static void main(String[] args) {
+        Client client = new Client();
+        //这里会立即返回，因为得到的是一个FutureData而不是RealData
+        Data data = client.request("name");
+        System.out.println("请求完毕...");
+        //
+//        SleepUtil.second(2);
+
+        System.out.println("data=" + data.getResult());
+    }
+}

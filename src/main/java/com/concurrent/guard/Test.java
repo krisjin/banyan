93f212d669d455ae0638fe51b0d8ae658d13ba88
krisjin <krisibm@163.com>
2017-05-15 23:30:20 +0800

    update guard

--- src/main/java/com/concurrent/guard/Test.java
+++ src/main/java/com/concurrent/guard/Test.java
@@ -4,6 +4,17 @@ package com.concurrent.guard;
  * User shijingui
  * Date 2017/5/14
  */
-public class Test
-{
+public class Test {
+
+    public static void main(String[] args) {
+        RequestQueue requestQueue = new RequestQueue();
+
+        for (int i = 0; i < 10; i++) {
+            new ServerThread(requestQueue, "ServerThread" + i).start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            new ClientThread(requestQueue, "ClientThread" + i).start();
+        }
+    }
 }

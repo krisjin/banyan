f09ed72e244d07d86fe5857ca056b07edf866d76
krisjin <krisibm@163.com>
2017-05-17 23:46:05 +0800

    create producer consumer pattern

---
+++ src/main/java/com/concurrent/producerconsumer/Consumer.java
@@ -0,0 +1,35 @@
+package com.concurrent.producerconsumer;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @author shijingui on 2017/5/17
+ */
+public class Consumer implements Runnable {
+    private BlockingQueue<String> queue;
+
+    public Consumer(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        Random r = new Random();
+        System.out.println("consumer thread is start...");
+        while (true) {
+            try {
+                String msg = queue.take();
+                if (msg != null) {
+
+                    System.out.println("consumer data =" + msg);
+                    Thread.sleep(r.nextInt(1000));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/concurrent/producerconsumer/Test.java
@@ -0,0 +1,37 @@
+package com.concurrent.producerconsumer;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * @author shijingui on 2017/5/17
+ */
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException {
+        BlockingDeque<String> queue = new LinkedBlockingDeque<>();
+
+        Producer p1 = new Producer(queue);
+        Producer p2 = new Producer(queue);
+
+        Consumer c1 = new Consumer(queue);
+        Consumer c2 = new Consumer(queue);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        executorService.execute(p1);
+        executorService.execute(p2);
+        executorService.execute(c1);
+        executorService.execute(c2);
+
+        Thread.sleep(1000);
+
+        p1.stop();
+        p2.stop();
+
+        executorService.shutdownNow();
+
+    }
+}

59baaa8570a796fa7d81b4c1e45f7c5e4b73f7e6
shijingui <shijingui@jd.com>
2019-03-05 22:03:04 +0800

    producer consumer queue

---
+++ src/main/java/test/Foobar.java
@@ -0,0 +1,23 @@
+package test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * User:shijingui
+ * Date:2019/3/5
+ *  
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Foobar implements Serializable {
+    private Long id;
+    private String name;
+    private int val;
+    private LocalDateTime dateTime;
+}

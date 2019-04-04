1d392edc8347a8bfecd67bbabe327ecca3754752
shijingui <shijingui@jd.com>
2019-04-04 08:56:00 +0800

    update md

--- src/main/java/org/banyan/bytecode/Syntest.java
+++ src/main/java/org/banyan/bytecode/Syntest.java
@@ -1,5 +1,7 @@
 package org.banyan.bytecode;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * User:shijingui
  * Date:2019/4/3
@@ -9,5 +11,30 @@ public class Syntest {
 
 
     public static synchronized void synTest() {
+    }
+
+    public void synTest2() {
+
+        synchronized (this) {
+            System.err.println(11);
+            aa();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        aa();
+    }
+
+    public static void aa() {
+
+        AtomicInteger num = new AtomicInteger(1);
+
+        num.compareAndSet(num.get(), 2);
+
+        num.incrementAndGet();
+        System.out.println();
+
     }
 }

24ef8ad4ed13a50428df441a373db2d6b2f9ee49
krisjin <krisibm@163.com>
2019-04-26 07:08:00 +0800

    spin lock

---
+++ src/main/java/org/banyan/concurrent/lock/SpinLock.java
@@ -0,0 +1,16 @@
+package org.banyan.concurrent.lock;
+
+/**
+ * <pre>
+ *   当竞争存在时，如果线程可以很快获得锁，那么可以不在OS层挂起线程，让线程做几个空操作（自旋）
+ *   JDK1.6中-XX:+UseSpinning开启
+ *   JDK1.7中，去掉此参数，改为内置实现
+ *   如果同步块很长，自旋失败，会降低系统性能
+ *   如果同步块很短，自旋成功，节省线程挂起切换时间，提升系统性能
+ *
+ * </pre>
+ *
+ * @author krisjin on 2019/4/26
+ */
+public class SpinLock {
+}

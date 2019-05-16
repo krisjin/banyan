651324f108f398edd3f215128196fa9e2c001207
krisjin <krisibm@163.com>
2019-05-16 21:49:34 +0800

    thread local

---
+++ src/main/java/org/banyan/concurrent/threadlocal/ThreadLocalConfinement.java
@@ -0,0 +1,28 @@
+package org.banyan.concurrent.threadlocal;
+
+/**
+ * Pattern: Thread Local Confinement
+ * <p>
+ * Motivation: Dealing with non thread-safe objects is a common situation, and
+ * it's possible to still have a thread-safe code even using non thread-safe
+ * classes.
+ * <p>
+ * Intent: Use ThreadLocal to confine instances in a per-thread model, keeping a
+ * object copy to each thread.
+ * <p>
+ * Applicability: Use for non thread-safe objects that needs to be shared across threads.
+ */
+public class ThreadLocalConfinement {
+
+    private static final ThreadLocal<Object> threadLocalOject = new ThreadLocal<Object>() {
+        @Override
+        protected Object initialValue() {
+            return new Object();
+        }
+    };
+
+    public Object getNowThreadSafeObjectInstance() {
+        return threadLocalOject.get();
+    }
+
+}
---
+++ src/main/java/org/banyan/concurrent/threadlocal/ThreadSafeDateFormat.java
@@ -0,0 +1,24 @@
+package org.banyan.concurrent.threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Pattern: Thread Local Confinement
+ * <p>
+ * Example: Using a thread safe SimpleDateFormat object
+ */
+public class ThreadSafeDateFormat {
+
+    private static final ThreadLocal<SimpleDateFormat> threadLocalDateFormat = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("DD/MM/YYYY HH:mm:ss");
+        }
+    };
+
+    public String format(Date date) {
+        return threadLocalDateFormat.get().format(date);
+    }
+
+}

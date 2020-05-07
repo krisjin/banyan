02496129c7eca3854c2099fcc1f24a311c1d350b
krisjin <krisibm@163.com>
2020-05-07 08:45:16 +0800

    final

---
+++ src/main/java/org/banyan/bytecode/Account.java
@@ -0,0 +1,22 @@
+package org.banyan.bytecode;
+
+/**
+ * User:krisjin
+ * Date:2020-05-07
+ */
+public class Account {
+
+
+    private final String name;
+    private double money;
+
+    //构造函数初始化
+    public Account() {
+        this.name = "zhangsan";
+        this.money = 111.11;
+    }
+
+
+
+
+}

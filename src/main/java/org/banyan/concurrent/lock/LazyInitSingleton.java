5c6174631c13be81fb0fc5761f36ca226f96df70
krisjin <krisibm@163.com>
2020-05-09 08:33:10 +0800

    init

--- src/main/java/org/banyan/concurrent/lock/LazyInitSingleton.java
+++ src/main/java/org/banyan/concurrent/lock/LazyInitSingleton.java
@@ -1,38 +1,33 @@
 package org.banyan.concurrent.lock;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * User:krisjin
  * Date:2020-05-08
  */
 public class LazyInitSingleton {
-    //    private volatile static LazyInitSingleton instance = null;
-    private static LazyInitSingleton instance = null;
+    private volatile static LazyInitSingleton instance = null;
 
     private LazyInitSingleton() {
     }
 
-    public synchronized static LazyInitSingleton getInstance() {
-        if (instance == null) {
-            instance = new LazyInitSingleton();
-        }
-        return instance;
-    }
-
-
-//    public static LazyInitSingleton getInstance() {
+//    public static synchronized LazyInitSingleton getInstance() {
 //        if (instance == null) {
-//            synchronized (LazyInitSingleton.class) {
-//                if (instance == null) {
-//                    instance = new LazyInitSingleton();
-//                }
-//            }
+//            instance = new LazyInitSingleton();
 //        }
 //        return instance;
 //    }
 
+
+    public static LazyInitSingleton getInstance() {
+        if (instance == null) {
+            synchronized (LazyInitSingleton.class) {
+                if (instance == null) {
+                    instance = new LazyInitSingleton();
+                }
+            }
+        }
+        return instance;
+    }
 
 
 }

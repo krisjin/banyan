ac9c8463d8c3eedf34b93380605c89ac833763c7
krisjin <krisibm@163.com>
2020-09-07 10:38:45 +0800

    update jieba test

---
+++ src/main/java/test/JiebaSegTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import com.huaban.analysis.jieba.JiebaSegmenter;
+import org.junit.Test;
+
+/**
+ * @User shijingui
+ * @date 2020/9/3
+ */
+public class JiebaSegTest {
+
+    @Test
+    public void test() {
+        JiebaSegmenter segmenter = new JiebaSegmenter();
+        String sentences = "习大大的演讲";
+        System.out.println(segmenter.sentenceProcess(sentences));
+    }
+
+}

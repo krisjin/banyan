7fa6dbb6cfa27c4cd5f205028a402336cf9ad7af
krisjin <krisibm@163.com>
2020-11-04 17:17:15 +0800

    update

--- src/main/java/org/banyan/concurrent/threadlocal/TLExam3.java
+++ src/main/java/org/banyan/concurrent/threadlocal/TLExam3.java
@@ -8,7 +8,7 @@ public class TLExam3 {
     private static ThreadLocal<String> tl = new ThreadLocal<>();
 
     public void setVal(String name) {
-        tl.set(name + ">" + Thread.currentThread().getName());
+        tl.set(Thread.currentThread().getName() + " -> " + name);
     }
 
     public String get1() {
@@ -23,17 +23,14 @@ public class TLExam3 {
         return tl.get();
     }
 
-
     public static void main(String[] args) {
         TLExam3 tlExam3 = new TLExam3();
-        tlExam3.setVal("kris");
+        tlExam3.setVal("tl");
         System.err.println(tlExam3.get1());
         System.err.println(tlExam3.get2());
         System.err.println(tlExam3.get3());
 
-
         //创建子线程
-
         new Thread(new ChildThread(tlExam3)).start();
     }
 
@@ -45,17 +42,13 @@ public class TLExam3 {
             this.tlExam3 = tlExam3;
         }
 
-        @Override
         public void run() {
-            tlExam3.setVal("chris");
+            tlExam3.setVal("kris");
             System.err.println(tlExam3.get1());
             System.err.println(tlExam3.get2());
-            tlExam3.setVal("chris2");
+            tlExam3.setVal("kris-2");//重新设值
             System.err.println(tlExam3.get3());
 
         }
-
-
     }
-
 }

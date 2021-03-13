56b2f9e98aa5255f4f29843e13690ef7d10e2c2c
krisjin <krisibm@163.com>
2021-03-13 22:17:26 +0800

    update test

--- src/main/java/org/banyan/concurrent/lock/CountDownLatchDemo.java
+++ src/main/java/org/banyan/concurrent/lock/CountDownLatchDemo.java
@@ -7,6 +7,10 @@ import java.util.concurrent.CountDownLatch;
  * 接着继续执行主线程
  * 倒计时锁存器的同步控制。
  * 使用AQS状态表示计数
+ * CountDownLatch类位于java.util.concurrent包下，利用它可以实现类似计数器的功能。比如有一个任务A，
+ * 它要等待其他4个任务执行完毕之后才能执行，此时就可以利用CountDownLatch来实现这种功能了。
+ * <p>
+ * CountDownLatch类只提供了一个构造器：
  *
  * @author krisjin
  * @date 2020/11/19

1aee58f251ffbd5d6bc11f9218c9a1f4b2b03cd4
krisjin <krisibm@163.com>
2021-10-03 10:52:43 +0800

    test

--- src/main/java/org/banyan/concurrent/interrupt/SleepInterrupt.java
+++ src/main/java/org/banyan/concurrent/interrupt/SleepInterrupt.java
@@ -5,18 +5,20 @@ package org.banyan.concurrent.interrupt;
  * Date: 2015/9/8
  */
 public class SleepInterrupt implements Runnable {
+
     public static void main(String[] args) {
         SleepInterrupt sleepInterrupt = new SleepInterrupt();
-        Thread thread = new Thread(sleepInterrupt);
-        thread.start();
+        Thread threadA = new Thread(sleepInterrupt);
+        threadA.start();
 
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         System.out.println("main() - interrupting other thread");
-        thread.interrupt();
+        threadA.interrupt();
         System.out.println("main() - end");
     }
 
@@ -27,8 +29,7 @@ public class SleepInterrupt implements Runnable {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             System.out.println("run() - interrupted while sleeping");
-            //处理完中断异常后，返回到run（）方法人口，
-            //如果没有return，线程不会实际被中断，它会继续打印下面的信息
+            //处理完中断异常后，返回到run（）方法人口，.如果没有return，线程不会实际被中断，它会继续打印下面的信息
             return;
         }
         System.out.println("run() - leaving normally");

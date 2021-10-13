cd3cf8d6b5517df46aa4f8e8ce6ce666c24b4e80
krisjin <krisibm@163.com>
2021-10-13 10:06:30 +0800

    test

--- src/main/java/org/banyan/concurrent/assembly/Test.java
+++ src/main/java/org/banyan/concurrent/assembly/Test.java
@@ -1,9 +1,5 @@
 package org.banyan.concurrent.assembly;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 /**
  * -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly
  *
@@ -14,24 +10,5 @@ public class Test {
 
     public static void main(String[] args) {
 
-
-        // 获得系统时间.
-        Date date = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM");
-        String nowTime = simpleDateFormat.format(date);
-        System.out.println(nowTime);
-
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(calendar.MONTH, -1);
-        SimpleDateFormat sdf = new SimpleDateFormat("2021-01");
-        String dateStr = sdf.format(calendar.getTime());
-
-        System.out.println(dateStr);
-
-        String ss="中国特产·当阳助农馆 ";
-
-
-        System.out.println(ss.trim());
     }
 }

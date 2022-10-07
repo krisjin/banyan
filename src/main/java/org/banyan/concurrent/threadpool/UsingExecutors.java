df698bfb2981bd36eb451427f599918c1e76d9a3
krisjin <krisibm@163.com>
2022-10-07 09:11:10 +0800

    update config

--- src/main/java/org/banyan/concurrent/threadpool/UsingExecutors.java
+++ src/main/java/org/banyan/concurrent/threadpool/UsingExecutors.java
@@ -15,7 +15,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class UsingExecutors {
 
     public static void main(String[] args) {
-        usingFixedThreadPool();
+        testThreadPool2();
     }
 
 

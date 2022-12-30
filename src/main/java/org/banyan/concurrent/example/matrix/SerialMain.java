2c397ed10db2a62975f6262a9c349884bc34cc2c
krisjin <krisibm@163.com>
2022-12-30 23:28:38 +0800

    clean

--- src/main/java/org/banyan/concurrent/example/matrix/SerialMain.java
+++ src/main/java/org/banyan/concurrent/example/matrix/SerialMain.java
@@ -8,8 +8,8 @@ public class SerialMain {
 
     public static void main(String[] args) {
 
-        double[][] matrix1 = MatrixGenerator.generate(2000, 2000);
-        double[][] matrix2 = MatrixGenerator.generate(2000, 2000);
+        double[][] matrix1 = MatrixGenerator.generate(10, 10);
+        double[][] matrix2 = MatrixGenerator.generate(10, 10);
 
         double[][] result = new double[matrix1.length][matrix2[0].length];
         long st = System.currentTimeMillis();

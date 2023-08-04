38676d07cae0896a1afb8dd3443bb6c00e77f023
krisjin <krisibm@163.com>
2023-08-04 23:52:47 +0800

    del

--- src/main/java/org/banyan/concurrent/base/computetask/Client.java
+++ src/main/java/org/banyan/concurrent/base/computetask/Client.java
@@ -5,24 +5,21 @@ import org.banyan.concurrent.util.SleepUtil;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * 这是什么
  */
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Computable computable = new ComputeDefault();
         ComputeWrapper<Integer, Integer> computeWrapper = new ComputeWrapper<Integer, Integer>(computable);
-        try {
-            int val = computeWrapper.compute(11);
-            System.out.println("第一次计算值:" + val);
-            SleepUtil.second(2);
-            val = computeWrapper.compute(11); //第二次从缓存去结果
-            System.out.println("第二次计算值:" + val);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        int val = computeWrapper.compute(11);
+        System.out.println("第一次计算值:" + val);
+        SleepUtil.second(2);
+        val = computeWrapper.compute(11); //第二次从缓存去结果
+        System.out.println("第二次计算值:" + val);
 
         Map<String, Object> m = new HashMap<String, Object>();
         Map<String, Object> m1 = new HashMap<String, Object>();

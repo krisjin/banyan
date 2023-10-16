b213f764ed9e57cdb1383dae0619bc8905f14abe
krisjin <krisibm@163.com>
2023-10-16 21:35:08 +0800

    add jvm test

--- src/main/java/org/banyan/concurrent/lock/SimpleLock.java
+++ src/main/java/org/banyan/concurrent/lock/SimpleLock.java
@@ -5,18 +5,15 @@ package org.banyan.concurrent.lock;
  * Date:2019/4/26
  */
 public class SimpleLock {
-
     private boolean isLocked = false;
 
     public synchronized void lock() throws InterruptedException {
-
         while (isLocked) {
             wait();
         }
         isLocked = true;
 
     }
-
 
     public synchronized void unlock() {
         isLocked = false;
